4062b08e52a61070cf658bfdb71d5a2ef1c2a06c
iramaroliveira <iramar.oliveira@trc.sebraeba.com.br>
2023-10-09 17:16:19 -0300

    Feito api de graficos

--- src/main/java/br/fvc/api/Controllers/DashboardController.java
+++ src/main/java/br/fvc/api/Controllers/DashboardController.java
@@ -4,9 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.fvc.api.Domain.Generic.GenericRequestDTO;
 import br.fvc.api.Services.DashboardService;
 
 @CrossOrigin
@@ -20,5 +24,15 @@ public class DashboardController {
     @GetMapping("/all")
     public ResponseEntity<Object> all() {
         return dashboardService.all();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> index(@PathVariable("id") Long id) {
+        return dashboardService.index(id);
+    }
+
+    @PostMapping("/filter")
+    public ResponseEntity<Object> filter(@RequestBody GenericRequestDTO data) {
+        return dashboardService.filter(data);
     }
 }
--- src/main/java/br/fvc/api/Services/DashboardService.java
+++ src/main/java/br/fvc/api/Services/DashboardService.java
@@ -1,9 +1,15 @@
 package br.fvc.api.Services;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import br.fvc.api.Domain.Generic.GenericRequestDTO;
 import br.fvc.api.Repositories.PaymentRepository;
 
 @Service
@@ -14,5 +20,25 @@ public class DashboardService {
 
     public ResponseEntity<Object> all() {
         return ResponseEntity.status(200).body(paymentRepository.getSumTotalValuesMonth());
+    }
+
+    public ResponseEntity<Object> index(Long id) {
+        return ResponseEntity.status(200).body(paymentRepository.reserveValueTotalMonth(id));
+    }
+
+    public ResponseEntity<Object> filter(GenericRequestDTO data) {
+        try {
+
+            SimpleDateFormat convertDate = new SimpleDateFormat("dd-MM-yyyy");
+            Date startDateConvert = convertDate.parse(data.start);
+            Date endtDateConvert = convertDate.parse(data.end);
+            ZoneId defaultZoneId = ZoneId.systemDefault();
+            LocalDate localDateOne = startDateConvert.toInstant().atZone(defaultZoneId).toLocalDate();
+            LocalDate localDateTwo = endtDateConvert.toInstant().atZone(defaultZoneId).toLocalDate();
+
+            return ResponseEntity.status(200).body(paymentRepository.filterInvoicingPeriod(localDateOne, localDateTwo));
+        } catch (Exception e) {
+            return ResponseEntity.status(400).body(e.getMessage());
+        }
     }
 }
